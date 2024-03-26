8d089b8bd85c6b59a920c341d14fea89828f6e72
kennethme9 <105962304+kennethme9@users.noreply.github.com>
2024-03-26 15:37:27 -0400

    Update mistral.py

--- unsloth/models/mistral.py
+++ unsloth/models/mistral.py
@@ -343,7 +343,7 @@ class FastMistralModel(FastLlamaModel):
         if load_in_4bit:
             bnb_config = BitsAndBytesConfig(
                 load_in_4bit              = True,
-                bnb_4bit_use_double_quant = True,
+                bnb_4bit_use_double_quant = False,
                 bnb_4bit_quant_type       = "nf4",
                 bnb_4bit_compute_dtype    = dtype,
             )
